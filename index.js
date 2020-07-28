c18c99963e09051872ef8de47fea3ea1f5c8388c
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-28 19:08:31 -0400

    .prompt questions

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-];
+
+// array of questions for user
+inquirer 
+    .prompt([
+        {
+        type: "input",
+        message: "What is your github?",
+        name: "github"
+        },
+        {
+        type: "input",
+        message: "What's your project title?",
+        name: "title"
+        }, 
+        {
+        type: "input",
+        message: "Please enter a description of your project.",
+        name: "description"
+        },
+
+]);
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+fs.writeToFile("README", document, function(err) {
+    
+});
 
 // function to initialize program
 function init() {
