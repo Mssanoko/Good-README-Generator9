680b48fd9c7015701d4459aebde6011564aa93de
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-28 21:29:12 -0400

    prompt question describe your project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
 const path = require("path");
+const { type } = require("os");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer
@@ -17,6 +18,21 @@ inquirer
     const queryUrl = `https://api.github.com/users/${username}`;
 
     axios.get(queryUrl).then(function(res) {
-        
+
     })
+  })
+  .then(function(){
+      inquirer.prompt([
+          {
+              type:"input",
+              message:"What is your project title?",
+              name:"title"
+          },
+          {
+              type:"input",
+              message:"Descripte your project",
+              name:"describe"
+          },
+
+      ])
   })
