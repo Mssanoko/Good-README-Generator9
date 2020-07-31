821a1b27a0286cd369e062909aef41869eaa46cf
Leslie Lawson <lawsonjleslie@gmail.com>
2020-07-31 08:04:07 -0400

    creation const for the doc

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const axios = require("axios");
+const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 inquirer.prompt (
@@ -15,6 +16,10 @@ inquirer.prompt (
 
     axios.get(queryUrl).then(function(res) {
         console.log(res.data);
+        const username = res.data.login;
+      const email = res.data.email;
+      const avatarUrl = res.data.avatar_url;
+      const avatar = `![Avatar](`+ avatarUrl +`)`;
         const doc = `username:` + res.data.login + `\n` + `email:` + res.data.email + `\n` + `![Avatar](`+ res.data.avatar_url +`)` + `\n`;
         fs.writeFile("readMe.md", doc, function(err) {
             if (err) {
@@ -25,17 +30,32 @@ inquirer.prompt (
   })
   .then(function(){
       inquirer.prompt([
-          {
+            {
+              type:"input",
+              message:"What is your username?",
+              name:"username"
+            },
+            {
+            type:"password",
+            message:"Type your password",
+            name:"password"
+            },
+            {
+                type:"password",
+                message:"Confirm your password",
+                name:"Confirm"
+            },
+            {
               type:"input",
               message:"What is your project title?",
               name:"title"
-          },
-          {
+            },
+            {
               type:"input",
               message:"Describe your project",
               name:"describe"
-          },
-          {
+            },
+            {
             type: "list",
             name: "project_license",
             message: "What kind of license applies to your project?",
@@ -46,6 +66,27 @@ inquirer.prompt (
                         "BSD 2", 
                         "APACHE 2.0", 
                         "None"]
-        },
+            },
       ])
-  })
+      .then(function(answers) {
+          var file = 
+          `## Table of Contents 
+
+          - [Description] (#description)`
+        
+          `## Description
+
+          ${answers.description}`
+        
+          `## Licenses`
+          
+
+          var doc = file 
+          fs.appendFile("readMe.md", doc, function(err){
+              if (err) {
+                  throw err;
+              }
+          })
+      })
+      });
+
---
+++ readMe.md
@@ -0,0 +1,3 @@
+username:Mssanoko
+email:null
+![Avatar](https://avatars2.githubusercontent.com/u/61078512?v=4)
